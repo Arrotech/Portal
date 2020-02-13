5713101e6cd0749f5958b80157dc10e8c16fb5f2
Arrotech <arrotechdesign@gmail.com>
2020-02-13 14:57:55 +0300

    Feature(User Details) Get User Details
    What does this pr do?
    
    Have a user able to get their information
    
    Any rlated pivotal stories?
    
    [Finishes #171254049]

--- UI/js/library.js
+++ UI/js/library.js
@@ -1,72 +1,57 @@
-    function callToast() {
+function callToast() {
 
-        var x = document.getElementById("snackbar");
-        x.className = "show";
-        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-    }
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+}
 
-    function onSuccess(msg){
+function onSuccess(msg) {
 
-        document.getElementById('snackbar').innerText = msg
-        callToast();
-    }
+    document.getElementById('snackbar').innerText = msg
+    callToast();
+}
 
-    function raiseError(msg){
+function raiseError(msg) {
 
-        document.getElementById('snackbar').innerText = msg
-        callToast();
-    }
+    document.getElementById('snackbar').innerText = msg
+    callToast();
+}
 
-    document.getElementById('getBooks').onclick = () => {
-            event.preventDefault();
+token = window.localStorage.getItem('token');
+admission = window.localStorage.getItem('admission_no');
 
-            token = window.localStorage.getItem('token');
-            admission = window.localStorage.getItem('admission_no');
+fetch('https://njc-school-portal.herokuapp.com/api/v1/books/' + admission, {
+    method: 'GET',
+    path: admission,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+    },
+})
+    .then((res) => res.json())
+    .then((data) => {
+        let status = data['status'];
+        let message = data['message'];
 
-            fetch('https://njc-school-portal.herokuapp.com/api/v1/books/' + admission ,{
-                method: 'GET',
-                path: admission,
-                headers : {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + token,
-                },
+        if (status === "200") {
+            var temp = "";
+            data.Book.forEach(book => {
+                temp += "<tr>";
+                temp += "<td>" + book.book_no + "</td>";
+                temp += "<td>" + book.author + "</td>";
+                temp += "<td>" + book.title + "</td>";
+                temp += "<td>" + book.subject + "</td>";
+                temp += "<td>" + book.form + "</td></tr>";
             })
-            .then((res) => res.json())
-            .then((data) => {
-                data.Book.forEach(book => {
-                    let status = data['status'];
-                    let message = data['message'];
-                    const { admission_no, book_no, author, title, subject, form } = book;
-                    output += `
-                        <div>
-                            <table>
-                                <tr>
-                                    <th>Book No.</th>
-                                    <th>Author</th>
-                                    <th>Title</th>
-                                    <th>Subject</th>
-                                    <th>Form</th>
-                                </tr>
-                                <tr>
-                                    <td>${book.book_no}</td>
-                                    <td>${book.author}</td>
-                                    <td>${book.title}</td>
-                                    <td>${book.subject}</td>
-                                    <td>${book.form}</td>
-                                </tr>
-                            </table>
-                        </div>
-                    `;
-                    if (status === '200'){
-                        document.getElementById('output').innerHTML = output;
-                    }else{
-                        raiseError(message);
-                    }
-                    });
-                    })
-            .catch((err)=>{
-                raiseError("Please check your internet connection and try again!");
-                console.log(err);
-            })
-    }
+            document.getElementById("data").innerHTML = temp;
+        }
+        else {
+            raiseError(message);
+        }
+
+    })
+    .catch((err) => {
+        raiseError("Please check your internet connection and try again!");
+        console.log(err);
+    })
