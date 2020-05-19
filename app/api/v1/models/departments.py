30dcf49e4431dccb7e3655b819b32ebab2056f9d
Arrotech <arrotechdesign@gmail.com>
2020-05-19 22:12:39 +0300

    Feature (Update Department)
    What does this pr do?
    
    Have an admin user able to update the department name.
    
    Any related pivotal stories?
    
    [Finishes #172884508]
    
    Screenshot

--- app/api/v1/models/departments.py
+++ app/api/v1/models/departments.py
@@ -1,15 +1,17 @@
+import json
 from datetime import datetime
 from app.api.v1.models.database import Database
 
+
 class DepartmentsModel(Database):
     """Departments model."""
-    
+
     def __init__(self, department_name=None, created_on=None):
         """Department constructor."""
         super().__init__()
         self.department_name = department_name
         self.created_on = datetime.now()
-        
+
     def save(self):
         """Add a new department."""
         self.curr.execute("""
@@ -20,17 +22,17 @@ class DepartmentsModel(Database):
         self.conn.commit()
         self.curr.close()
         return response
-    
+
     def get_department_name(self, department_name):
         """Get department by name."""
         self.curr.execute("""
-                          SELECT * FROM departments WHERE department_name=%s
-                          """,(department_name,))
+                          SELECT * FROM departments WHERE department_name='{}'
+                          """.format(department_name))
         response = self.curr.fetchone()
         self.conn.commit()
         self.curr.close()
         return response
-    
+
     def get_all_departments(self):
         """Fetch all departments."""
         self.curr.execute("""
@@ -40,11 +42,21 @@ class DepartmentsModel(Database):
         self.conn.commit()
         self.curr.close()
         return response
-    
+
     def get_department_by_id(self, department_id):
         """Fetch a specific department by id."""
-        self.curr.execute("""SELECT * FROM departments WHERE department_id={}""".format(department_id))
+        self.curr.execute(
+            """SELECT * FROM departments WHERE department_id={}""".format(department_id))
         response = self.curr.fetchall()
+        self.conn.commit()
+        self.curr.close()
+        return response
+
+    def edit_department(self, department_id, department_name):
+        """Update department name."""
+        self.curr.execute(
+            """UPDATE departments SET department_name='{}' WHERE department_id={} RETURNING department_name""".format(department_id, department_name))
+        response = self.curr.fetchone()
         self.conn.commit()
         self.curr.close()
         return response
--- app/api/v1/views/departments.py
+++ app/api/v1/views/departments.py
@@ -1,3 +1,4 @@
+import json
 from flask import request
 from flask_jwt_extended import jwt_required
 from utils.serializer import Serializer
@@ -15,7 +16,7 @@ def add_department():
     details = request.get_json()
     errors = check_department_keys(request)
     if errors:
-        return Serializer.serialize(errors, 400, "Invalid {} key".format(', '.join(errors)))
+        return raise_error(400, "Invalid {} key".format(', '.join(errors)))
     department_name = details['department_name']
     if DepartmentsModel().get_department_name(department_name):
         return raise_error(400, "{} department already exists".format(department_name))
@@ -30,6 +31,7 @@ def get_all_departments():
     response = DepartmentsModel().get_all_departments()
     return Serializer.serialize(response, 200, 'Departments successfully retrieved')
 
+
 @departments_v1.route('/departments/<int:department_id>', methods=['GET'])
 @jwt_required
 def get_department_by_id(department_id):
@@ -37,4 +39,22 @@ def get_department_by_id(department_id):
     response = DepartmentsModel().get_department_by_id(department_id)
     if response:
         return Serializer.serialize(response, 200, 'Department successfully retrieved')
+    return raise_error(404, "Department not found")
+
+
+@departments_v1.route('/departments/<int:department_id>', methods=['PUT'])
+@jwt_required
+@admin_required
+def update_department(department_id):
+    """Update a department by id."""
+    details = request.get_json()
+    errors = check_department_keys(request)
+    if errors:
+        return raise_error(400, "Invalid {} key".format(', '.join(errors)))
+    department_name = details['department_name']
+    if DepartmentsModel().get_department_name(department_name):
+        return raise_error(400, "{} department already exists".format(department_name))
+    response = DepartmentsModel().edit_department(department_name, department_id)
+    if response:
+        return Serializer.serialize(response, 200, 'Department updated successfully')
     return raise_error(404, "Department not found")
