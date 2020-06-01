6da0c32bd9936fbf50a48dcb0c119c2f2d5cd431
Arrotech <arrotechdesign@gmail.com>
2020-06-01 14:08:45 +0300

    Feature (Update Certificate)
    What does this pr do?
    
    Have an admin user able to update a certificate.
    
    Any related pivotal stories?
    
    [Finishes #173098275]
    
    Screenshot

--- app/api/v1/views/certificates.py
+++ app/api/v1/views/certificates.py
@@ -43,3 +43,22 @@ def get_certificate_by_id(certificate_id):
     if response:
         return Serializer.serialize(response, 200, "Certificate retrieved successfully")
     return raise_error(404, "Certificate not found")
+
+
+@certificates_v1.route('/certificates/<int:certificate_id>', methods=['PUT'])
+@jwt_required
+@admin_required
+def update_certificate(certificate_id):
+    """Update a certificate by id."""
+    details = request.get_json()
+    errors = check_certificates_keys(request)
+    if errors:
+        return raise_error(400, "Invalid {} key".format(', '.join(errors)))
+    details = request.get_json()
+    certificate_name = details['certificate_name']
+    if not certificate_restrictions(certificate_name):
+        return raise_error(400, "Invalid certificate name")
+    response = CertificatesModel().edit_certificate(certificate_name, certificate_id)
+    if response:
+        return Serializer.serialize(response, 200, 'Certificate updated successfully')
+    return raise_error(404, "Certificate not found")
--- tests/test_certificates.py
+++ tests/test_certificates.py
@@ -1,6 +1,7 @@
 import json
 
-from utils.dummy import new_certificate, certificate_keys, certificate_name_restrictions
+from utils.dummy import new_certificate, certificate_keys, certificate_name_restrictions,\
+    update_certificate, update_certificate_keys, update_certificate_name_restrictions
 from .base_test import BaseTest
 
 
@@ -75,3 +76,56 @@ class TestCertificates(BaseTest):
         self.assertEqual(result['message'],
                          'Certificate not found')
         assert response2.status_code == 404
+        
+    def test_update_certificate(self):
+        """Test that an admin can update an existing certificate."""
+        response1 = self.client.post(
+            '/api/v1/certificates', data=json.dumps(new_certificate), content_type='application/json',
+            headers=self.get_admin_token())
+        response2 = self.client.put(
+            '/api/v1/certificates/1', data=json.dumps(update_certificate), content_type='application/json',
+            headers=self.get_admin_token())
+        result = json.loads(response2.data.decode())
+        self.assertEqual(result['message'],
+                         'Certificate updated successfully')
+        assert response2.status_code == 200
+        
+    def test_update_certificate_keys(self):
+        """Test that an admin cannot update an existing certificate with invalid json keys."""
+        response1 = self.client.post(
+            '/api/v1/certificates', data=json.dumps(new_certificate), content_type='application/json',
+            headers=self.get_admin_token())
+        response2 = self.client.put(
+            '/api/v1/certificates/1', data=json.dumps(update_certificate_keys), content_type='application/json',
+            headers=self.get_admin_token())
+        result = json.loads(response2.data.decode())
+        self.assertEqual(result['message'],
+                         'Invalid certificate_name key')
+        assert response2.status_code == 400
+        
+    def test_update_certificate_name_restrictions(self):
+        """Test that an admin cannot update an existing certificate with invalid certificate name."""
+        response1 = self.client.post(
+            '/api/v1/certificates', data=json.dumps(new_certificate), content_type='application/json',
+            headers=self.get_admin_token())
+        response2 = self.client.put(
+            '/api/v1/certificates/1', data=json.dumps(update_certificate_name_restrictions), content_type='application/json',
+            headers=self.get_admin_token())
+        result = json.loads(response2.data.decode())
+        self.assertEqual(result['message'],
+                         'Invalid certificate name')
+        assert response2.status_code == 400
+        
+    def test_update_non_existing_certificate(self):
+        """Test that an admin cannot update non existing certificate."""
+        response1 = self.client.post(
+            '/api/v1/certificates', data=json.dumps(new_certificate), content_type='application/json',
+            headers=self.get_admin_token())
+        response2 = self.client.put(
+            '/api/v1/certificates/10', data=json.dumps(update_certificate), content_type='application/json',
+            headers=self.get_admin_token())
+        result = json.loads(response2.data.decode())
+        self.assertEqual(result['message'],
+                         'Certificate not found')
+        assert response2.status_code == 404
+        
