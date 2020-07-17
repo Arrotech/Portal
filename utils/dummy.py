ffcf184236a5d80eaa1e1ef5f33dff0b071d50b4
Harun Gachanja <arrotechdesign@gmail.com>
2020-07-17 22:54:12 +0300

    Feature (Update Institution)
    What does this pr do?
    
    Have an admin able to update an exiting institution.
    
    Any related pivotal stories?
    
    [Finishes #173648956]

--- utils/dummy.py
+++ utils/dummy.py
@@ -937,3 +937,10 @@ new_institution = {
 institution_keys = {
     "institutioname": "Jomokenyatta University of Agriculture and Technology"
 }
+
+update_institution = {
+    "institution_name": "Kenyatta University"
+}
+update_institution_keys = {
+    "institutioname": "Kenyatta University"
+ }
