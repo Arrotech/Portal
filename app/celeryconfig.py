69240af31bc1fc8602ac96cd6f1b7767df80ddc0
Harun Gachanja <arrotechdesign@gmail.com>
2020-12-09 12:25:19 +0300

    Feature (Configure Redis)
    What does this pr do?
    
    Install and configure redis on heroku.

--- app/celeryconfig.py
+++ app/celeryconfig.py
@@ -5,7 +5,7 @@ from app.__init__ import exam_app
 def make_celery(app):
     celery = Celery(
         app.import_name,
-        broker="amqp://localhost//"
+        broker="redis://localhost:6379/0"
     )
     celery.conf.update(app.config)
 
